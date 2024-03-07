905d6d4c364805d364f553708b0cdbbb9d43671c
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-03-07 19:30:35 +0530

    work1

--- 1.c
+++ 1.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 void main()
 {
-    printf("hello");
+    float a,b,c;
+    printf("enter the price :");
+    scanf("%f",&a);
+    printf("enter the price :");
+    scanf("%f",&b);
+
+    c=(a*b)/100;
+
+    printf("The calculates the discount :%f",c);
+
+
 }
