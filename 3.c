594648e3821ad6e1440e2ebaab9e270d2b375445
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-03-07 19:43:13 +0530

    work3

---
+++ 3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    int a=65,b=45,c;
+
+    printf("Enter the first value : %d\n",a);
+    
+
+    printf("Enter the second value :%d\n",b);
+   
+
+    c=180-(a+b);
+
+    printf("Tiangle of : %d",c);
+}
