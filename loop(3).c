d0dc326db9658d28779b9b2180ade768bd277617
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-03-15 15:35:43 +0530

    loop

---
+++ loop(3).c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int a,i=0;
+    printf("enter the value :-");
+    scanf("%d",&a);
+  
+  while(i<=a){
+      printf("%d\n",i);
+      i++;
+  }
+   
+
+    return 0;
+}
