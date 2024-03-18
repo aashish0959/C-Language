e5981531ace641a405d3bb45e9c2769be61df4d2
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-03-18 15:36:46 +0530

    looping-2

---
+++ looping-2(3).c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int f=0 ,s=1,n;
+
+    for(int i=0; i<=0; i++);
+    {
+        printf("%d",f);
+        n=f+s;
+        f=s;
+        s=n;
+    }
+    
+ 
+
+}
