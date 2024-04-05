17da12d72840065861169acc401fc010f70f10a6
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-04-05 15:59:33 +0530

    string-2

--- string-2/1.c
+++ string-2/1.c
@@ -1,17 +1,10 @@
+#include<stdio.h>
 
-#include <stdio.h>
-
-int main() {
-   char val[]="HELLO WORD";
-   for(int i=0;val[i]!=NULL;i++){
-      if(val[i]>='A' && val[i]<='Z'){
-          val[i]+=32;
-      }
-      printf("%c",val[i]);
-   }
-
-    return 0;
+main(){
+ char n[100];
+ printf("enter the name string :");
+ gets(n);
+ puts(strupr(n));
+ 
+	
 }
-
-
-
--- string-2/2.c
+++ string-2/2.c
@@ -1,41 +1,7 @@
-
-#include <stdio.h>
-
-int main() {
-   char val[]="AAshish";
-   for(int i=0;val[i]!=NULL;i++){
-      if(val[i]>='A' && val[i]<='Z'){
-          val[i]+=32;
-      }
-      else if
-      (val[i]>='a' && val[i]<='z'){
-          val[i]-=32;
-      } 
-      printf("%c",val[i]);
-      
-   }
-    
-
-    return 0;
+#include<stdio.h>
+main(){
+	char n[100];
+	printf("enter the  string :");
+	gets (n);
+	puts(strlwr(n));
 }
-
-// user get string 
-
-// #include<stdio.h>
-// main(){
-//     char name[100];
-//     printf("Enter Any String:- ");
-//     scanf("%s",name);
-//     for (int i = 0; name[i] !='\0'; i++)
-//     {
-//         if (name[i] >='A'&& name[i] <='Z')
-//         {
-//             name[i]+=32;
-//         }
-//         else if (name[i] >='a'&& name[i] <='z')
-//         {
-//             name[i]-=32;
-//         }
-//     }
-//     printf("%s",name);
-// }
