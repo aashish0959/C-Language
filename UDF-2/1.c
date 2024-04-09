ee95e39877710aa0c1a3f14465f1a41f9830c6c5
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-04-09 15:46:40 +0530

    UDF-2

---
+++ UDF-2/1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+                 
+ int sum(int arr[])
+{
+    int sum;
+    for (int i=0;i<=4;i++){
+        sum +=arr[i];
+    }
+   printf("%d",sum);
+}
+
+int main() {
+  int arr[]={6,4,1,5,2};
+    sum(arr);
+
+}
