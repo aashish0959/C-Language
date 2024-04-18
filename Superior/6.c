b00bc8b919584ec9aaa37f364494160b810875ad
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-04-18 15:52:09 +0530

    superior

--- Superior/6.c
+++ Superior/6.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    int arr1[100], arr2[100], merged[200], n1, n2, mergedSize;
+    int array1[100], array2[100], mergedArray[200], n1, n2, mergedSize;
 
-   
+    // Input for the first array
     printf("Enter the number of elements in the first array: ");
     scanf("%d", &n1);
 
     printf("Enter %d elements for the first array:\n", n1);
     for (int i = 0; i < n1; i++) {
-        scanf("%d", &arr1[i]);
+        scanf("%d", &array1[i]);
     }
 
- 
+    // Input for the second array
     printf("Enter the number of elements in the second array: ");
     scanf("%d", &n2);
 
     printf("Enter %d elements for the second array:\n", n2);
     for (int i = 0; i < n2; i++) {
-        scanf("%d", &arr2[i]);
+        scanf("%d", &array2[i]);
     }
 
-    
+    // Merging arrays
     mergedSize = n1 + n2;
     for (int i = 0; i < n1; i++) {
-        merged[i] = arr1[i];
+        mergedArray[i] = array1[i];
     }
     for (int i = 0; i < n2; i++) {
-        merged[n1 + i] = arr2[i];
+        mergedArray[n1 + i] = array2[i];
     }
 
-
+    // Output the merged array
     printf("Merged array: ");
     for (int i = 0; i < mergedSize; i++) {
-        printf("%d ", merged);
+        printf("%d ", mergedArray[i]);
     }
     printf("\n");
 
